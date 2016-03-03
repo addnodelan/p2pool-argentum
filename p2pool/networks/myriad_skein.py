90a34472fd9c4f7757a4b3d52b00b58aa654e648
nzsquirrell <nzsquirrell@users.noreply.github.com>
2016-03-03 17:05:41 +1300

    myriad p2pool reboot

--- p2pool/networks/myriad_skein.py
+++ p2pool/networks/myriad_skein.py
@@ -12,12 +12,12 @@ CHAIN_LENGTH = 24*60*60//10 # shares
 REAL_CHAIN_LENGTH = 24*60*60//10 # shares
 TARGET_LOOKBEHIND = 200 # shares
 SPREAD = 60 # blocks
-IDENTIFIER = 'fafa54457667eeee'.decode('hex')
-PREFIX = 'fa6754ee45ee76fa'.decode('hex')
+IDENTIFIER = '78ce490bee6d24ff'.decode('hex')
+PREFIX = 'c13cb62e90e67a7a'.decode('hex')
 P2P_PORT = 5588
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
 PERSIST = True
 WORKER_PORT = 5589
-BOOTSTRAP_ADDRS = 'birdspool.no-ip.org nz.nutty.one mine4free.noip.me tanukifu.ddns.net'.split(' ')
+BOOTSTRAP_ADDRS = 'birdspool.no-ip.org nz.nutty.one'.split(' ')
 VERSION_CHECK = lambda v: None if 90216 <= v else 'Myriad version too old. Upgrade to 0.9.2.16 or newer!'
