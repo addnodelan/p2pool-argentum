996ee286ffa2431fd856f54a15b3f8cef88a31bd
nzsquirrell <squirrell@nutty.one>
2016-07-07 20:18:41 +1200

    add new p2pool network settings for Myriad-Yescrypt

---
+++ p2pool/networks/myriad_yescrypt.py
@@ -0,0 +1,24 @@
+from p2pool.bitcoin import networks
+
+# CHAIN_LENGTH = number of shares back client keeps
+# REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
+# REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
+# REAL_CHAIN_LENGTH must be changed in sync with all other clients
+# changes can be done by changing one, then the other
+
+PARENT = networks.nets['myriad_yescrypt']
+SHARE_PERIOD = 15 # seconds
+CHAIN_LENGTH = 24*60*60//15 # shares
+REAL_CHAIN_LENGTH = 24*60*60//15 # shares
+TARGET_LOOKBEHIND = 200 # shares
+SPREAD = 60 # blocks
+IDENTIFIER = '11b421519e6007d1'.decode('hex')
+PREFIX = 'a68b7bd6c223f853'.decode('hex')
+P2P_PORT = 5533
+MIN_TARGET = 0
+MAX_TARGET = 2**256//10000 - 1
+PERSIST = False
+WORKER_PORT = 5534
+BOOTSTRAP_ADDRS = 'nz.nutty.one'.split(' ')
+VERSION_CHECK = lambda v: None if 90216 <= v else 'Myriad version too old. Upgrade to 0.9.2.16 or newer!'
+ALGORITHM = 'Yescrypt'
