07311b8f59c8388248c10ea1482cf65b4a9653c1
protonn <support@argentum.io>
2018-02-07 17:58:31 -0500

    add lyra2re2

---
+++ hash_modules/lyra2re-hash-python/README.md
@@ -0,0 +1,13 @@
+lyra2re-hash-python
+=====================
+
+Python module with Vertcoin's hashing algorithm used by some other tools
+
+Credit goes to https://github.com/metalicjames/lyra2re-hash-python
+
+cd lyra2re-hash-python
+git submodule init
+git submodule update
+sudo python setup.py install
+cd ../
+python run_p2pool.py --net argentum_lyra2re2
--- p2pool/networks/argentum_sha256.py
+++ p2pool/networks/argentum_sha256.py
@@ -13,7 +13,7 @@ REAL_CHAIN_LENGTH = 24*60*60//10 # shares
 TARGET_LOOKBEHIND = 200 # shares
 SPREAD = 40 # blocks
 IDENTIFIER = '52b2a82e80126f49'.decode('hex')
-PREFIX = 'e0780b8d18363f5'.decode('hex')
+PREFIX = '720089174edc7a34'.decode('hex')
 P2P_PORT = 13586
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**32 - 1
@@ -24,3 +24,4 @@ BOOTSTRAP_ADDRS = '45.76.112.155 45.76.240.8'.split(' ')
 VERSION_CHECK = lambda v: None if 1090000 <= v else 'Argentum version too old. Upgrade to 4.14.3 or newer!'
 VERSION_WARNING = lambda v: None
 MINIMUM_PROTOCOL_VERSION = 1500
+ALGORITHM = 'Sha256'
